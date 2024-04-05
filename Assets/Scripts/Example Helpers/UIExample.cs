79e54f3a89d5c616b2c81c9206b07e0ffa282082
Martin Pluisch <31780571+mapluisch@users.noreply.github.com>
2024-04-05 19:52:44 +0200

    Update to latest LTS + new UI demo

--- Assets/Scripts/Example Helpers/UIExample.cs
+++ Assets/Scripts/Example Helpers/UIExample.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,10 +11,56 @@ public class UIExample : MonoBehaviour
     [SerializeField] private Text speedLabel;
     [SerializeField] private InputField input;
     [SerializeField] private TTSManager ttsManager;
-    
+    [SerializeField] private Image entityImage;
+    private Coroutine talkingEffect;
     public void OnTalkButton()
     {
-        if (ttsManager) ttsManager.SynthesizeAndPlay(input.text, (TTSModel) modelDropdown.value, (TTSVoice) voiceDropdown.value, speedSlider.value);
+        if (ttsManager)
+        {
+            ttsManager.SynthesizeAndPlay(input.text, (TTSModel) modelDropdown.value, (TTSVoice) voiceDropdown.value, speedSlider.value);
+            talkingEffect ??= StartCoroutine(TalkingEffect());
+        }
+    }
+    
+    // hacky little animation to "mimic" a talking effect of the 2d person sprites
+    IEnumerator TalkingEffect()
+    {
+        yield return new WaitForSeconds(1f);
+        
+        Vector2 initialPosition = entityImage.rectTransform.anchoredPosition;
+        Quaternion initialRotation = entityImage.rectTransform.rotation;
+    
+        float duration = 1.0f;
+        float offset = 50f;
+        float time = 0;
+
+        Vector2 lastPosition = initialPosition;
+        Quaternion lastRotation = initialRotation;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            
+            float targetOffsetX = UnityEngine.Random.Range(-offset, offset); 
+            float targetOffsetY = UnityEngine.Random.Range(-offset, offset); 
+            float targetRotationZ = UnityEngine.Random.Range(-offset, offset); 
+
+            Vector2 targetPosition = initialPosition + new Vector2(targetOffsetX, targetOffsetY);
+            Quaternion targetRotation = Quaternion.Euler(0, 0, targetRotationZ) * initialRotation;
+
+            entityImage.rectTransform.anchoredPosition = Vector2.Lerp(lastPosition, targetPosition, 0.005f);
+            entityImage.rectTransform.rotation = Quaternion.Lerp(lastRotation, targetRotation, 0.005f);
+
+            lastPosition = entityImage.rectTransform.anchoredPosition;
+            lastRotation = entityImage.rectTransform.rotation;
+
+            yield return null;
+        }
+
+        entityImage.rectTransform.anchoredPosition = initialPosition;
+        entityImage.rectTransform.rotation = initialRotation;
+
+        talkingEffect = null;
     }
 
     public void UpdateSpeedLabel(Single value)
